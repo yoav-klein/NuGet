5bc5ccaa0d47f5d711374b9c7c93d33145863929
Yoav <yoavklein25@gmail.com>
2022-09-08 18:56:24 +0300

    Functions

---
+++ create-package/using-nuget-cli/Functions.ps1
@@ -0,0 +1,19 @@
+
+
+function Build-Project() {
+	# first, build the package
+	& "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+}
+
+function Gen-Nuspec() {
+	../../nuget.exe spec
+}
+
+function Pack() {
+	../../nuget.exe pack .
+}
+
+function Push() {
+       $APIKEY=$(cat apikey)
+	../../nuget.exe push -Source "py" -Apikey $APIKEY using-nuget-cli.2.4.1.nupkg
+}
